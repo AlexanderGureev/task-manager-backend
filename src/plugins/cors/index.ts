1b6e0339e08f65441a915c51360fbafbb5322c41
alexandergureev <g.alex00@bk.ru>
2019-09-08 18:08:58 +0300

    fix cors

--- src/plugins/cors/index.ts
+++ src/plugins/cors/index.ts
@@ -8,7 +8,7 @@ export const cors = {
     await server.register({
       plugin: require("hapi-cors"),
       options: {
-        origins: [config.CLIENT_URL],
+        origins: [config.CLIENT_URL, "https://api-hapi-todo.herokuapp.com"],
         allowCredentials: "true",
         methods: ["POST, GET, PATCH, PUT, OPTIONS, DELETE"]
       }
